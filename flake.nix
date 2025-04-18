965c98fbf972671d5c2100d25f6cc40f3e5e2577
Adam Brady <adam@boxxen.org>
2025-04-19 00:52:29 +1000

    flake.nix for developer environment

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url     = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ pkgs.nodejs_23 pkgs.screen ];
+          shellHook = ''
+            pushd ${p}
+              npm install
+            popd
+          '';
+        };
+
+        dbxSessionName = "dpanel";
+        dbxStartCommand = "npm start";
+        dbxDevCwd = "dev";
+      }
+    );
+}

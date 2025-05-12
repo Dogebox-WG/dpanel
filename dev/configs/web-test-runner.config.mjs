43dadba68b5589f5fc1f49fac8dc31784609b59d
Ando <divinegod@gmail.com>
2025-05-12 11:10:30 +1000

    add playwright to flake

--- dev/configs/web-test-runner.config.mjs
+++ dev/configs/web-test-runner.config.mjs
@@ -15,8 +15,8 @@ export default {
     }
   },
   browsers: [
-    // playwrightLauncher({ product: 'chromium' }),
-    playwrightLauncher({ product: 'firefox' }),
+    playwrightLauncher({ product: 'chromium' }),
+    // playwrightLauncher({ product: 'firefox' }),
     // playwrightLauncher({ product: 'webkit' })
   ],
   testRunnerHtml: testFramework => `

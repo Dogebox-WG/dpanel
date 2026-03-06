c3cef2b102f071e219a9fef6f294e0e6b0bf7b0a
Ben <elusiveshiba@gmail.com>
2026-03-06 16:01:50 +1100

    Removing deliberate fail test used in testing, undo changes to web-test-runner previously used to suppress a warning for a test that's now being skipped

--- src/components/common/dynamic-form/tests/deliberate-fail.test.js
+++
@@ -1,7 +0,0 @@
-import { expect } from "../../../../../dev/node_modules/@open-wc/testing";
-
-describe("Deliberate failure (remove this test)", () => {
-  it("should fail on purpose to test Discord notifications", () => {
-    expect(true).to.equal(false);
-  });
-});
